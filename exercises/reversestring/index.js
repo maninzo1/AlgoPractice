c4d60acdb7c9cc925dc8eef6e1aaa4b7e3cbc3c2
MaryAninzo <aninzom211@gmail.com>
2022-02-10 20:13:53 -0500

    done

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,26 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let reversed = '';
+
+    for (let character of str){
+        reversed = character + reversed
+    }
+
+    return reversed
+ 
+}
+
+reverse('hello world')
+
 
 module.exports = reverse;
+
+
+   // let reversed = '';
+
+    // for (let i = str.length - 1; i >= 0; i--){
+    //     reversed += str[i];
+    // }
+    // return reversed;
