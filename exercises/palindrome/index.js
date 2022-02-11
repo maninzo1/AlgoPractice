4dc2f4add9bd2a1b2c53c4691b60bcc73e4a2060
MaryAninzo <aninzom211@gmail.com>
2022-02-11 12:05:18 -0500

    done

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,18 +8,22 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-  let reversed = '';
-  
-  for (let character of str){
-      reversed = character + reversed
-  }
-  if (reversed === str){
-      return true
-  }else{
-      return false
-  }
+    return str.split('').every((char, i) =>{
+        return char === str[str.length -i -1];
+    });
 }
 
 palindrome('hello')
 
 module.exports = palindrome;
+
+//   let reversed = '';
+  
+//   for (let character of str){
+//       reversed = character + reversed
+//   }
+//   if (reversed === str){
+//       return true
+//   }else{
+//       return false
+//   }
